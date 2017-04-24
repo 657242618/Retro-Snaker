85bf5b8e4271ececbba5106dc84f2e85b1f92664
657242618 <657242618@qq.com>
2017-04-24 17:18:58 +0800

    Add files via upload

---
+++ BodyMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyMove : MonoBehaviour
+{
+    public GameObject next;                        //下一节身体
+
+ //  public float speed;
+ //   public float rotateSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+/*    public void moveTo(Vector3 pos)
+    {//移动到前一位置
+            Vector3 old = this.transform.position;  //为下一节身体记录位置
+            this.transform.position = pos;          //移动到pos
+            if (next != null)                       //如果有下一节身体
+            {
+                next.GetComponent<BodyMove>().moveTo(old);//下一节身体继续移动
+            }
+
+    }
+ */   
+
+}
+
---
+++ FollowMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FollowMovement : MonoBehaviour
+{
+    public GameObject nextBody;
+
+    public float RecordGap;         //目标移动多远记录一次距离,数值越小距离越近，路径越接近头的运动轨迹
+    public float WalkSpeed;         //走速度
+    public float RunSpeed;          //跑速度
+    public float SpeedLerpRant;     //速度变化的缓动率
+    public int StartRunCount;       //距离目标多远后开始跑（单位是List的Item数）
+
+    public Transform TargetTF;      //要跟随的目标
+
+    public bool Running
+    {   
+        //访问器
+        get { return PosList.Count > StartRunCount; }
+        set { }
+    }
+
+
+    private Rigidbody Rig;
+    private List<Vector3> PosList = new List<Vector3>();
+
+
+
+    void Awake()
+    {
+        Rig = GetComponent<Rigidbody>();
+    }
+
+
+        void FixedUpdate()
+        {
+
+            if (TargetTF)
+            {
+                //清除已经到达的点
+                if (PosList.Count > 0 && Vector3.Distance(transform.position, PosList[0]) < RecordGap)
+                {
+                    PosList.RemoveAt(0);
+                }
+                if (PosList.Count > 0)
+                {
+                    //添加当前Target位置
+                    if (Vector3.Distance(TargetTF.position, PosList[PosList.Count - 1]) > RecordGap)
+                    {
+                        PosList.Add(TargetTF.position);
+                    }
+                    //处理移动
+                    if (PosList.Count > 0)
+                    {//更新velocity
+                        Rig.velocity = Vector3.Lerp(Rig.velocity, new Vector3(PosList[0].x - transform.position.x, 0, PosList[0].z - transform.position.z).normalized * (Running ? RunSpeed : WalkSpeed), 1);
+                        //Lerp（）得到两个向量的线性差值p，p=form+(to-form)*t   t[0,1]
+                }
+                else
+                {
+                    
+                    Rig.velocity = Vector3.Lerp(Rig.velocity, Vector3.zero, SpeedLerpRant);
+                }
+            }
+            else
+            {
+                //生成新节点的时候后续节点停止运动
+                Rig.Sleep();
+                //记录新的点
+                PosList.Add(TargetTF.position);
+            }
+        }
+
+
+    }
+
+}
